126c85bc4f5dc40248303bf9ede112ef0bc25bd7
Hemang Desai <hd@hemangs-mbp.lan>
2022-04-21 21:19:57 -0400

    used useState

--- react-challenge/src/Home.js
+++ react-challenge/src/Home.js
@@ -1,20 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import Astro from './astro.jpg';
 import "./home.css";
 
 function Home() {
+    const [doggo, setDoggo] = useState(false)
 
-    function setDoggo() {
-        const dog = document.getElementById('astro__image');
-        if (dog.style.display === 'none') {
-            dog.style.display = 'block';
-            dog.style.height = "50%"
-        } else {
-            dog.style.display = "none"
-            dog.style.height = "50%"
-        }
+    const handleToggle = () => {
+        setDoggo(!doggo)
     }
+
     return (
         <div>
         <Helmet>
@@ -22,8 +17,8 @@ function Home() {
         <meta name="my lovely doggo" content="the goldenator" />
         <meta name="keywords" content="Image of my best friend!" />
         </Helmet>
-        <button onClick={() => setDoggo()}>Doggo</button>
-        <img id="astro__image" alt='doggo' src={Astro}></img>
+        <button onClick={handleToggle}>Doggo</button>
+        {doggo ? (<img id="astro__image" alt='doggo' src={Astro}></img>) : (<p></p>)}
         </div>
     )
 }
